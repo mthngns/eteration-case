95dbd46f8c3cd55a8b8a44798563921c44526a1a
Mete_Work_Mac <mthngns78@gmail.com>
2024-08-23 23:08:41 +0300

    feat: Created navigation logic and page

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,18 +1,67 @@
 "use client";
+
+import ErrorCard from "@/app/components/ErrorCard/ErrorCard";
+import Loader from "@/app/components/Loader/Loader";
+import ProductDetailCard from "@/app/components/ProductDetailCard/ProductDetailCard";
+import ShoppingMenu from "@/app/features/ShoppingMenu/ShoppingMenu";
+import { Product } from "@/app/lib/types";
+import {
+  addItemToBasket,
+  getBasket,
+} from "@/redux/features/basket/store/basket";
+import { useGetProductByIdQuery } from "@/redux/features/products/services/products";
+import { useAppDispatch } from "@/redux/store";
 import React from "react";
+import { useSelector } from "react-redux";
 
 const ProductDetail = ({ params }: { params: { id: string } }) => {
   const productId = params.id;
 
+  const dispatch = useAppDispatch();
+  const {
+    data: product,
+    isLoading,
+    isError,
+  } = useGetProductByIdQuery(productId);
+
+  const basket = useSelector(getBasket);
+  const basketProductIds = new Set(
+    basket.productList.map((product) => product.id)
+  );
+
+  const handleAddItemToBasket = (product: Product) => {
+    const isProductInBasket = basket.productList.some(
+      (item) => item.id === product.id
+    );
+    if (!isProductInBasket) {
+      dispatch(addItemToBasket({ ...product, pcs: "1" }));
+    }
+  };
+  if (isLoading) {
+    return <Loader />;
+  }
+  if (isError) {
+    return <ErrorCard />;
+  }
+  if (product) {
     return (
       <div className="flex w-full h-full justify-center py-0">
         <div
           className={`flex flex-col w-11/12 gap-4 py-4 lg:grid lg:grid-cols-12 gap-x-4`}
         >
-          {productId}
+          <div className="flex flex-col h-fit order-3 lg:order-2 lg:col-span-9 2xl:col-span-10 bg-white shadow-lg">
+            <ProductDetailCard
+              product={product}
+              addToCard={(product) => handleAddItemToBasket(product)}
+              isExist={basketProductIds.has(product.id)}
+              isLoading={isLoading}
+            />
+          </div>
+          <ShoppingMenu />
         </div>
       </div>
     );
+  }
 };
 
 export default ProductDetail;
